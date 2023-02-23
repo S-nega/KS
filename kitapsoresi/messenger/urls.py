b86f3ec8ebcb81d95d08f410c6f3e6d8c756c706
s-nega <snezhana.golovko@narxoz.kz>
2023-02-23 09:30:45 +0600

    fixed templates

--- kitapsoresi/messenger/urls.py
+++ kitapsoresi/messenger/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('registeration/', registrationPage, name='registrationPage'),
+    path('registration/', registrationPage, name='registrationPage'),
     path('cats/<int:catid>/', categories, name='cats'),  # http://127.0.0.1:8000/library/cats/
 ]
