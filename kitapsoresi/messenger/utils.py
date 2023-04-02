e08969752fd0ee986235459c07dfe27939fe851c
s-nega-m <117503114+s-nega-m@users.noreply.github.com>
2023-04-02 21:23:43 +0600

    authorization, registration, cache, toolbar

--- kitapsoresi/messenger/utils.py
+++ kitapsoresi/messenger/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Count
+from django.core.cache import cache
 
 from .models import *
 
@@ -7,22 +8,34 @@ menu = [
     {'title': "Поисковик", 'url_name': '/searchPage'},
     {'title': "Добавить книгу", 'url_name': '/addBookPage'},
     {'title': "О нас", 'url_name': '/about'},
-    {'title': "Войти", 'url_name': '/login'},
 ]
 
 class DataMixin:
     paginate_by = 3
     def get_user_context(self, **kwargs):
         context = kwargs
-        genres = Genre.objects.annotate(Count('books'))
-        # authors = Author.objects.annotate(Count('books'))
-
         user_menu = menu.copy()
+        genres = cache.get('genres')
+        cache_menu = cache.get('menu')
+
         if not self.request.user.is_authenticated:
             user_menu.pop(2)
+            cache.set('menu', user_menu, 60)
 
-        context['menu'] = user_menu
+        if self.request.user.is_authenticated:
+            user_menu = menu.copy()
+            cache.set('menu', user_menu, 60)
 
+        if not cache_menu:
+            cache.set('menu', user_menu, 60)
+
+        if not genres:
+            genres = Genre.objects.annotate(Count('books'))
+            cache.set('genres', genres, 60)
+        # authors = Author.objects.annotate(Count('books'))
+
+
+        context['menu'] = cache_menu
         context['genres'] = genres
         # context['authors'] = authors
         if 'genre_selected' not in context:
